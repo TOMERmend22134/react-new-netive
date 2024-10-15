18843c159ddfede8a1f458e28f52c90adb82dc2b
TOMERmend22134 <123461274+TOMERmend22134@users.noreply.github.com>
2024-10-15 11:00:03 +0300

    15.10.2024
    יצירה של העבודה של יעל מחדש

---
+++ pages/UploadFiles.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function UploadFiles() {
+  return (
+    <View>
+      <Text>UploadFiles</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
---
+++ pages/ViewProfile.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function ViewProfile() {
+  return (
+    <View>
+      <Text>ViewProfile</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
